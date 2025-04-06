e5f439919e48f0757e7557ada0945cb3adb5d981
kennedyifeadi <kennedyifeadi@gmail.com>
2025-04-06 22:47:55 +0100

    Refactor App component to add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "@fontsource/poppins"; // Defaults to weight 400
 import "@fontsource/poppins/300.css"; // Light
 import "@fontsource/poppins/600.css"; // Semi-Bold
 import "@fontsource/poppins/700.css"; // Bold
+import { Footer } from "./components/layout/Footer";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Footer/>
     </div>
   );
 }
