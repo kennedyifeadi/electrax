561efd549d1d39b9c095aaebce2ff5e5ceef2723
kennedyifeadi <kennedyifeadi@gmail.com>
2025-04-07 19:19:39 +0100

    Refactor Footer component for improved responsiveness

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,6 +2,39 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='w-full h-[120dvh] bg-[#f1f1f1] flex flex-col p-4 max-h-max'>
+        <div className='w-full h-[30%] max-h-[36dvh] flex items-center lg:flex-row flex-col'>
+            <div>
+                <h1></h1>
+                <p></p>
+            </div>
+            <div>
+                <form action="">
+                    <input type="text" />
+                    <button></button>
+                </form>
+                <span>
+
+                </span>
+            </div>
+        </div>
+        <div className='flex flex-wrap w-full h-[60%] max-h-max'>
+            <div className='w-full lg:w-[40%]'></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <div className='w-full h-[10%] max-h-[12dvh] flex justify-between items-center md:flex-row flex-col'>
+            <div>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    </div>
   )
 }
