d3b88e89bf2722a173c7dafafa6b1fe36d4a0690
kennedyifeadi <kennedyifeadi@gmail.com>
2025-04-09 00:40:26 +0100

    Refactor Footer component to update newsletter section and improve styling

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,19 +3,19 @@ import { NavLink } from 'react-router-dom'
 
 export const Footer = () => {
   return (
-    <div className='w-full bg-[#f1f1f1] flex flex-col p-4 h-max'>
-        <div className='w-full h-max border border-black flex items-center lg:flex-row flex-col'>
-            <div>
-                <h1 className='text-3xl text-[#333]'>Join our newsletter</h1>
-                <p>We’ll send you a nice letter once per week. No spam.</p>
+    <div className='w-full bg-[#f1f1f1] flex flex-col p-4 h-max' style={{fontFamily:" 'Inter', sans-serif"}}>
+        <div className='w-full h-max flex lg:flex-row flex-col px-4'>
+            <div className="w-full h-[20dvh] md:h-[28dvh] lg:h-[32dvh] md:w-[60%] lg:w-[50%] flex flex-col gap-4 justify-center">
+                <h1 className='text-2xl md:text-3xl lg:text-4xl font-semibold text-[#333]'>Join our newsletter</h1>
+                <p className='text-[15px] md:text-[18px] text-[#333333d5]'>We’ll send you a nice letter once per week. No spam.</p>
             </div>
-            <div>
-                <form action="">
-                    <input type="text" />
-                    <button></button>
+            <div className='w-full h-[24dvh] md:h-[28dvh] lg:h-[32dvh] md:w-[70%] lg:w-[50%] flex flex-col gap-4 justify-center'>
+                <form action="" className='w-full h-[70%] md:h-[30%] lg:h-[25%] flex md:flex-row flex-col gap-4'>
+                    <input type="text" className='w-full md:w-[70%] h-[50%] md:h-full px-4 rounded-md border border-[#d4d4d4]' placeholder='Enter your email'/>
+                    <button className='w-full md:w-[20%] h-[50%] md:h-full rounded-md border border-[#d4d4d4] bg-transparent hover:bg-[#d4d4d4] duration-500 ease-in-out text-black flex justify-center items-center text-[15px]'>Subscribe</button>
                 </form>
-                <span>
-
+                <span className='text-[15px] md:text-[18px] text-[#333333d5]'>
+                    We care about your data in our privacy policy.
                 </span>
             </div>
         </div>
