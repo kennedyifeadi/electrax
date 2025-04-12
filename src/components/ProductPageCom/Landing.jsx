a652e60591292c23145d706e882fea031f74f29f
kennedyifeadi <kennedyifeadi@gmail.com>
2025-04-12 01:37:27 +0100

    Refactor Landing component to add a dynamic background image and flex layout to main container

--- src/components/ProductPageCom/Landing.jsx
+++ src/components/ProductPageCom/Landing.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { DynamicBackground } from '../ui/DynamicBackground'
+import landingImage from "../../assets/Images/plane black background.avif"
 
 export const Landing = () => {
   return (
-    <div>Landing</div>
+    <div className='w-full h-[150dvh] bg-[#000000] relative'>
+        <DynamicBackground image={landingImage} size='70%' />
+        <div className='w-full h-full relative flex flex-col'></div>
+    </div>
   )
 }
