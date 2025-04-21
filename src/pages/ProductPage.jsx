3b2190c840f12a743bbb28c39242606762ff48d7
kennedyifeadi <kennedyifeadi@gmail.com>
2025-04-21 09:55:33 +0100

    Refactor ProductPage to add ParralaxSection component

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Landing } from '../components/ProductPageCom/Landing'
 import { Aereodynamics } from '../components/ProductPageCom/Aereodynamics'
+import { ParralaxSection } from '../components/ProductPageCom/ParralaxSection'
 
 export const ProductPage = () => {
   return (
     <div className='flex flex-col w-full h-max'>
       <Landing/>
       <Aereodynamics/>
+      <ParralaxSection/>
     </div>
   )
 }
