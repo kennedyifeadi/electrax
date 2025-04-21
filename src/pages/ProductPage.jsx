a235220e8b7d963a4deca6cf1cae91b83f9f1546
kennedyifeadi <kennedyifeadi@gmail.com>
2025-04-21 09:50:34 +0100

    Refactor ProductPage to add Aereodynamics component

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Landing } from '../components/ProductPageCom/Landing'
+import { Aereodynamics } from '../components/ProductPageCom/Aereodynamics'
 
 export const ProductPage = () => {
   return (
     <div className='flex flex-col w-full h-max'>
       <Landing/>
+      <Aereodynamics/>
     </div>
   )
 }
