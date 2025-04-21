aae13c4ab4d4b8d56c54c71dafc5d649efad01c0
Bright Ihegawam <bright06iheagwam@gmail.com>
2025-04-21 11:49:36 +0100

    section three done

--- src/pages/careers.jsx
+++ src/pages/careers.jsx
@@ -2,6 +2,25 @@ import plane1 from "../assets/Images/careers.avif";
 import values from "../assets/Images/values abstract 2.avif";
 import Carousel from "../components/layout/carousel";
 import Button from "../components/ui/Buttons";
+const coreValues = [{
+  title:"Innovation",
+  body:"We push the boundaries of aviation, integrating advanced technology to enhance performance and safety."
+},{
+  title:"Craftsmanship",
+  body:"Our focus on precision engineering and attention to detail ensures top-tier quality in every aircraft."
+},{
+  title:"Heritage",
+  body:"We honor aviation’s legacy by blending classic design with modern engineering for a timeless flying experience."
+},{
+  title:"Excellence",
+  body:"We strive for excellence in every detail, from the materials we use to the performance of our planes."
+},{
+  title:"Customer-Centricity",
+  body:"Our customers are our priority. We provide tailored solutions and exceptional service to meet their needs."
+},{
+  title:"Performance-Driven",
+  body:"Every decision we make is geared toward maximizing the performance and reliability of our aircraft."
+},]
 export const Careers = () => {
   return (
     <div className="">
@@ -24,19 +43,32 @@ export const Careers = () => {
       </div>
       {/* section 2 */}
       <div className=" px-8 py-20 w-full flex flex-col gap-8">
-        <h1 className="capitalize text-[2.5rem] font-semibold text-[#333333] px- ">We make cool things</h1>
+        <h1 className="capitalize text-[2.5rem] font-semibold text-[#333333] px- ">
+          We make cool things
+        </h1>
         <Carousel />
       </div>
       {/* section 3 */}
-      <div className="grid grid-cols-2">
-        <img src={values} alt="core values" />
-        <div>
-            <h1>Our Core Values</h1>
-            <section>
-              <span>
-                 <img src={values} alt="core values" />
+      <div className="grid grid-cols-2 h-screen px-8 ">
+        <span className="flex  items-center">
+          <img src={values} alt="core values" className=" rounded-[2rem] w-[80%] h-[98%] " />
+
+        </span>
+        <div className="mt-10 flex flex-col gap-8 ">
+          <h1 className="font-semibold text-[2.6rem]">Our Core Values</h1>
+          <section className="grid grid-cols-2 gap-4 ">
+            {coreValues.map((item, index) => (
+              <span key={index} className="flex flex-col gap-2 mt-8">
+                <h1 className="font-semibold text-[1.3rem] ">{item.title}</h1>
+                <p className="text-[#333333] text-[1.1rem] font-semibold">
+                  {item.body}
+                </p>
               </span>
-            </section>
+            ))}
+            
+            
+           
+          </section>
         </div>
       </div>
     </div>
